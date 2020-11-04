4a2c1d3867c035fd7461670b0abeb8a4c8db14a7
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-11-04 18:52:13 +0200

    updates demo screen

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'This is an element created via JS';
-
-  return element;
-}
-
-document.body.appendChild(component());
